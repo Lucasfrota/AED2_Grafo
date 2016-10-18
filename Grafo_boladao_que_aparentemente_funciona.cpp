6e8d5204e63b395eb28d05e097d2c07a9515ba20
Lucasfrota <frota_lv@hotmail.com>
2016-10-18 12:14:46 -0400

    Update Grafo_boladao_que_aparentemente_funciona.cpp

--- Grafo_boladao_que_aparentemente_funciona.cpp
+++ Grafo_boladao_que_aparentemente_funciona.cpp
@@ -12,6 +12,104 @@ enum Cor{
 	cinza
 };
 
+template <class T>
+class Item{
+	private:
+		T valor;
+		Item *prox;
+	public:
+		T getValor(){
+			return this->valor;
+		}
+		Item* getProx(){
+			return prox;
+		}
+		void setValor(T valor){
+			this->valor = valor;
+		}
+		void setProx(Item *item){
+			this->prox = item;
+		}
+};
+
+template <class T>
+class Fila{
+	private:
+		T *items;
+		T *frente;
+		T *tras;
+		int tam;
+		int tamAbsoluto;
+		
+	public:
+		
+		void cria(int tam){
+			this->items = (T*) malloc(sizeof(T) * tam);
+			
+			for(int i = 0; i < tam; i++)
+				items[i].setValor(-1);
+			
+			this->frente = items;
+			this->tras = items;
+			this->tamAbsoluto = tam;
+			this->tam = 0; 
+		}
+		
+		bool vazia(){
+			return (this->tam == 0);
+		}
+		
+		void enfileira(T item){
+			cout << "Valor interno: " << item.getValor() << "\n";
+			this->tam++;
+			this->tras++;
+			this->tras->setValor(item.getValor());
+			cout << "Tras: " << tras->getValor() << "\n";
+			//items[1].setValor(item.getValor());//CUIDADO! Gambiarra violenta nao mantenha distancia!
+			tras->setProx(NULL);
+		}
+		
+		int desenfileira(){
+			
+			T item;
+			
+			int saida = items[1].getValor();
+			
+			item = *frente;
+			
+			frente = frente->getProx();
+			
+			desaloque(this->items);
+			
+			this->tam--;
+			
+			return saida;
+		}
+		
+		void mostra(){
+			for(int i = 1; i < tam+1; i++)
+				cout << items[i].getValor() << " | ";
+			cout << endl;
+		}
+		
+		int getTam(){
+			return this->tam;
+		}
+		
+		void setTam(int tam){
+			this->tam = tam;
+		}
+		
+		void desaloque(T items[]){
+			T aux[this->tamAbsoluto];
+			
+			for(int i = 0; i < tamAbsoluto; i++)
+				aux[i] = items[i];
+			
+			for(int i = 1; i < tamAbsoluto; i++)
+				items[i-1] = aux[i];
+		}
+};
 
 template <class T>
 class Vertice{
@@ -97,7 +195,7 @@ class Vertice{
 
 
 template <class T>
-class ListaVertice{
+class Lista{
 	private:
 		T lista[100];
 		int cont = 0;
@@ -131,14 +229,24 @@ class ListaVertice{
 		
 		void print(){
 			for(int i = 0; i < cont; i++){
-				cout << lista[i].getValor() <<"\n";
+				cout << lista[i] <<"\n";//.getValor() <<"\n";
 			}
 		}
 		
+		void retiraUltimo(){
+			lista[cont] = NULL;
+			cont--;
+		}
+		
+		int getTam(){
+			return cont;
+		}
+		
+		/*
 		T* getLista(){
 			return &lista[100];
 		}
-		
+		*/
 };
 
 template <class T>
@@ -147,7 +255,7 @@ class Graph{
 		int ordem;
 		int tamanho;
 		int tempo;
-		ListaVertice<T> lista;
+		Lista<T> lista;
 	public:
 		Graph(int ordem){
 			tempo = 0;
@@ -184,7 +292,7 @@ class Graph{
 			
 		}
 		
-		ListaVertice<T>* getListaVertices(){
+		Lista<T>* getListaVertices(){
 			return &lista;
 		}
 		
@@ -221,10 +329,50 @@ class Graph{
 			cout << "\n-----" << u->getValor() << "---- FECHA\n";
 		}
 		
+		void BFS(Vertice<Cor> *s){
+			Fila< Item<int> > Q;
+			int u;
+			int *listaAdj;
+			
+			Q.cria(100);
+			for(int i = 0; i < getOrdem(); i++){
+				lista.getVerticeEm(i)->setCor(branco);
+				lista.getVerticeEm(i)->setPi(NULL);
+				lista.getVerticeEm(i)->setD(-1);//colocar um define pra infinito mais tarde
+			}
+			getVerticeDe(s->getValor())->setCor(cinza);
+			getVerticeDe(s->getValor())->setD(0);
+			getVerticeDe(s->getValor())->setPi(NULL);
+			Item<int> aux;
+			aux.setValor(s->getValor());
+			Q.enfileira(aux);
+			while(Q.getTam() > 0){
+				Q.mostra();
+				u = Q.desenfileira();
+				listaAdj = getVerticeDe(u)->getAdjI();
+//				cout << "antes do loop " << u << "\n";
+				for(int i = 0; i < getVerticeDe(u)->getQtddVertice();i++){
+//					cout << "Hello World!\n";
+					if(getVerticeDe(listaAdj[i])->getCor() == branco){
+						getVerticeDe(listaAdj[i])->setCor(cinza);
+						getVerticeDe(listaAdj[i])->setD(getVerticeDe(getVerticeDe(u)->getValor())->getD() + 1);
+						getVerticeDe(listaAdj[i])->setPi(getVerticeDe(u));
+						aux.setValor(getVerticeDe(listaAdj[i])->getValor());
+						cout << "-> " << aux.getValor() << "\n";
+						Q.enfileira(aux);
+						cout << "-->";
+						Q.mostra();
+					}	
+				}
+				getVerticeDe(u)->setCor(preto);
+			}
+		}
+		
 		void cores(){//isso aparentemente prova que isso funciona
 			getVerticeDe(1)->getCor();
 			getVerticeDe(2)->getCor();
 			getVerticeDe(3)->getCor();
+			
 		}
 		
 		
@@ -239,6 +387,7 @@ int main(int argc, const char * argv[]) {
 	a.insertEdge(1, 2);
 	a.insertEdge(2, 3);
 	a.insertEdge(3, 1);
+	
 	
 	//teste 2
 	/*
@@ -262,7 +411,6 @@ int main(int argc, const char * argv[]) {
 	*/
 
 	
-	a.DFS();
-	a.cores();
-	
+	a.BFS(a.getVerticeDe(1));
+	//a.cores();	
 }
